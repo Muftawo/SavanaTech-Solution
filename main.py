28babc8a902d324ae568e85f98651f47d366c29f
Muftawo <hajjmuf@gmail.com>
2024-02-13 14:54:54 +0000

    formatted code using black

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import logging
 from github import Github
 from src.extract import Extract
@@ -8,31 +7,33 @@ from src.transform import transformer
 def setup_logging():
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
 
-def main()-> None:
-        
-    org_name = 'Scytale-exercise'
+
+def main() -> None:
+
+    org_name = "Scytale-exercise"
 
     g = Github()
     org = g.get_organization(org_name)
-    
-    #paginated list
+
+    # paginated list
     repositories = org.get_repos()
 
-    logging.info('Begin data extraction...')
+    logging.info("Begin data extraction...")
     for repo in repositories:
         repo_name = repo.name
         repository_data = Extract.get_repo_data(repo)
         Extract.save_repo_data(org_name, repo_name, repository_data)
     logging.info(f"Data extracted form {repositories.totalCount} repos")
-    logging.info('Extraction completed !!')
+    logging.info("Extraction completed !!")
 
-    logging.info('Begin Trasnformation...')
+    logging.info("Begin Trasnformation...")
     transformer()
-    logging.info('Transformation Done.')
-    
+    logging.info("Transformation Done.")
+
+
 if __name__ == "__main__":
     main()
--- src/extract.py
+++ src/extract.py
@@ -3,38 +3,40 @@ import json
 
 
 class Extract:
-    def get_repo_data(repo:str) -> dict:
-        """This function retreives all data from a public github repository passed to it 
+    def get_repo_data(repo: str) -> dict:
+        """This function retreives all data from a public github repository passed to it
 
         Args:
-            repo (str): name of the github repository 
+            repo (str): name of the github repository
 
         Returns:
-            dict: contain key data about the repository and all pull request submmited. 
+            dict: contain key data about the repository and all pull request submmited.
         """
-        repo_data ={"org": repo.full_name,
-                    "name":repo.name ,
-                    "id": repo.id, 
-                    "login":repo.owner.login}
+        repo_data = {
+            "org": repo.full_name,
+            "name": repo.name,
+            "id": repo.id,
+            "login": repo.owner.login,
+        }
         prs = []
 
-        for pull_request in repo.get_pulls(state='all'):
+        for pull_request in repo.get_pulls(state="all"):
             pull_request_info = {
-                'title': pull_request.title,
-                'number': pull_request.number,
-                'state': pull_request.state,
-                'created_at': str(pull_request.created_at),
-                'updated_at': str(pull_request.updated_at),
+                "title": pull_request.title,
+                "number": pull_request.number,
+                "state": pull_request.state,
+                "created_at": str(pull_request.created_at),
+                "updated_at": str(pull_request.updated_at),
             }
             prs.append(pull_request_info)
-        
-        repo_data["pull_requests"]=prs
-        
+
+        repo_data["pull_requests"] = prs
+
         return repo_data
 
-    def save_repo_data(org_name:str, repo_name:str, repository_data:str) -> None:
+    def save_repo_data(org_name: str, repo_name: str, repository_data: str) -> None:
         """saves data of a repository to a respective data path, each repository gets its own sub directory.
-        dir structure 
+        dir structure
         Orgnaization -|--repo--data.json
                         -|--repo--data.json
         Args:
@@ -42,7 +44,7 @@ class Extract:
             repo_name (str): repository name
             repository_data (str): _description_
         """
-        
+
         org_folder = f"{org_name}"
         repo_folder = f"{org_name}/repos/{repo_name}"
 
@@ -53,6 +55,5 @@ class Extract:
             os.makedirs(repo_folder)
 
         file_path = f"{repo_folder}/data.json"
-        with open(file_path, 'w') as json_file:
+        with open(file_path, "w") as json_file:
             json.dump(repository_data, json_file, indent=4)
-
