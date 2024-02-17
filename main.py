2b91d57265a48ffb61ed539593f3dfae623357b4
Muftawo <hajjmuf@gmail.com>
2024-02-17 22:06:52 +0000

    moved orgnizations github  initialization  to extract file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import logging
-from github import Github
 from src.extract import Extract
 from src.transform import transformer
 
@@ -17,18 +16,11 @@ def main() -> None:
 
     org_name = "Scytale-exercise"
 
-    g = Github()
-    org = g.get_organization(org_name)
-
-    # paginated list
-    repositories = org.get_repos()
-
+    
     logging.info("Begin data extraction...")
-    for repo in repositories:
-        repo_name = repo.name
-        repository_data = Extract.get_repo_data(repo)
-        Extract.save_repo_data(org_name, repo_name, repository_data)
-    logging.info(f"Data extracted form {repositories.totalCount} repos")
+    
+    Extract.extract_and_save(org_name)
+   
     logging.info("Extraction completed !!")
 
     logging.info("Begin Trasnformation...")
